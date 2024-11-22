28b20ed7f8b4423f519ed3215625fb67c244b702
yjzhang <yjzhang@cs.washington.edu>
2024-11-21 20:31:11 -0800

    testing

--- test/test_bmatrix.py
+++ test/test_bmatrix.py
@@ -5,7 +5,11 @@ class BNTest(unittest.TestCase):
 
     def setUp(self):
         # paths to initial networks
-        self.network1 = ''
+        self.network1 = 'input_files/simple_network_test.txt'
 
     def test_load_equation(self):
         "Tests loading equations"
+        boolean_network = BMatrix.make_boolean_network(self.network1)
+
+if __name__ == '__main__':
+    unittest.main()
